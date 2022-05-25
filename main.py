801fd33a2c45e601aaffcfa89bdd7da7d5cc4ca2
Daniil Solntsev <105362198+Solntsev-D@users.noreply.github.com>
2022-05-25 23:08:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from selenium import webdriver
-import time
 from selenium.webdriver.common.by import By
+from datetime import date
 import json
+import time
 
-url_list = [
-    'https://www.nytimes.com/search?dropmab=true&query=&sections=World%7Cnyt%3A%2F%2Fsection%2F70e865b6-cc70-5181-84c9-8368b3a5c34b&sort=best',
-    'https://www.nytimes.com/search?dropmab=true&query=&sections=U.S.%7Cnyt%3A%2F%2Fsection%2Fa34d3d6c-c77f-5931-b951-241b4e28681c&sort=best',
-    'https://www.nytimes.com/search?dropmab=true&query=&sections=New%20York%7Cnyt%3A%2F%2Fsection%2F39480374-66d3-5603-9ce1-58cfa12988e2&sort=best',
-    'https://www.nytimes.com/search?dropmab=true&query=&sections=Sports%7Cnyt%3A%2F%2Fsection%2F4381411b-670f-5459-8277-b181485a19ec&sort=best'
-]
+url_list = {
+    'https://www.nytimes.com/search?dropmab=true&query=&sections=World%7Cnyt%3A%2F%2Fsection%2F70e865b6-cc70-5181-84c9-8368b3a5c34b&sort=best': 'World',
+    'https://www.nytimes.com/search?dropmab=true&query=&sections=U.S.%7Cnyt%3A%2F%2Fsection%2Fa34d3d6c-c77f-5931-b951-241b4e28681c&sort=best': 'U.S.',
+    'https://www.nytimes.com/search?dropmab=true&query=&sections=New%20York%7Cnyt%3A%2F%2Fsection%2F39480374-66d3-5603-9ce1-58cfa12988e2&sort=best': 'NewYork',
+    'https://www.nytimes.com/search?dropmab=true&query=&sections=Sports%7Cnyt%3A%2F%2Fsection%2F4381411b-670f-5459-8277-b181485a19ec&sort=best': 'Sports'
+}
 
 index = 0
 for url in url_list:
@@ -29,35 +30,23 @@ for url in url_list:
 
         titles_articles = driver.find_elements(By.CSS_SELECTOR, 'h4.css-2fgx4k')
         release_dates = driver.find_elements(By.CSS_SELECTOR, 'span.css-17ubb9w')
-        locations = driver.find_elements(By.CSS_SELECTOR, 'p.css-myxawk')
+        Categories = driver.find_elements(By.CSS_SELECTOR, 'p.css-myxawk')
         urls_articles = driver.find_elements(By.TAG_NAME, 'a')
 
         for i in range(0, len(titles_articles)):
+            release_date = release_dates[i].text
+            if 'ago' in release_date:
+                release_date = str(date.today())
             data = {
-                'Date: ': release_dates[i].text,
-                'Type:': locations[i].text,
+                'Date: ': release_date,
+                'Category:': Categories[i].text,
                 'Title:': titles_articles[i].text,
                 'Url:': urls_articles[i].get_attribute('href')
             }
-            if index == 1:
-                filename = 'world_section'+str(i)+'.json'
-                with open(filename, 'w') as f:
-                    json.dump(data, f)
 
-            elif index == 2:
-                filename = 'ua_section'+str(i)+'.json'
-                with open(filename, 'w') as f:
-                    json.dump(data, f)
-
-            elif index == 3:
-                filename = 'newyork_section'+str(i)+'.json'
-                with open(filename, 'w') as f:
-                    json.dump(data, f)
-
-            elif index == 4:
-                filename = 'sport_section'+str(i)+'.json'
-                with open(filename, 'w') as f:
-                    json.dump(data, f)
+            filename = url_list[url] + '.json'
+            with open(filename, 'a') as f:
+                f.write(json.dumps(data, indent=4))
 
     except Exception as ex:
         print(ex)
